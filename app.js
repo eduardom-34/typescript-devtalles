d0a24398e30e489b3004e61004ed60a8cd85b5b9
cesar <eduardomelchor34gmail.com>
2024-06-16 14:42:25 -0600

    seccion4 done

--- app.js
+++ app.js
@@ -6,18 +6,18 @@ function sumar(a, b) {
 const contar = (heroes) => {
     return heroes.length;
 };
-const superHeroes = ["Flash", "Arrow", "Superman", "Linterna Verde"];
+const superHeroes = ['Flash', 'Arrow', 'Superman', 'Linterna Verde'];
 contar(superHeroes);
 //Parametros por defecto
-const llamarBatman = (llamar) => {
+const llamarBatman = (llamar = true) => {
     if (llamar) {
-        console.log("Batiseñal activada");
+        console.log('Batiseñal activada');
     }
 };
 llamarBatman();
 // Rest?
-const unirheroes = (personas) => {
-    return personas.join(", ");
+const unirheroes = (...personas) => {
+    return personas.join(', ');
 };
 // Tipo funcion
 const noHaceNada = (numero, texto, booleano, arreglo) => { };
