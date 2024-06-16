729afc5f4d61f492543235b667798a33617f7cfa
cesar <eduardomelchor34gmail.com>
2024-06-16 16:30:14 -0600

    seccion5 before homework

--- objetos/objects.ts
+++ objetos/objects.ts
@@ -1,5 +1,23 @@
 (() => {
-  let flash = {
+  let flash: {
+    name: string;
+    age?: number;
+    powers: number[];
+    getName?: () => string | undefined;
+  } = {
     name: 'Barry Allen',
+    age: 24,
+    powers: [1, 2],
+  };
+
+  let superman: {
+    name: string;
+    age?: number;
+    powers: string[];
+    getName?: () => string | undefined;
+  } = {
+    name: 'Clark Kent',
+    age: 60,
+    powers: ['Super velocidad'],
   };
 })();
--- objetos/type.js
+++ objetos/type.js
@@ -3,11 +3,14 @@
     let flash = {
         name: 'Barry Allen',
         age: 24,
-        powers: [1, 2],
+        powers: ['Super velocidad', 'viajar en el tiempo'],
     };
     let superman = {
         name: 'Clark Kent',
         age: 60,
         powers: ['Super velocidad'],
+        getName() {
+            return this.name;
+        },
     };
 })();
