ae0f48bd34c9fe264667b2dcdc7a7d22543c86f1
cesar <eduardomelchor34gmail.com>
2024-06-19 20:01:16 -0600

    Seccion12 Done

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import { getPokemon } from "./generics/get-pokemos";
-import { Pokemon } from './interfaces/pokemon';
+import { Pokemon } from "./decorators/pokemon-class";
 
 
-getPokemon( 4 )
-    .then( pokemon => console.log ( pokemon.sprites.front_default ))
-    .catch( error => console.error( error ))
-    .finally( () => console.log('Fin de getPokemon'))
+
+const charmander = new Pokemon('Charmander');
+
+// (Pokemon.prototype as any).customName = 'Pikachu';
+
+// charmander.savePokemonToDB( 300 );
+charmander.publicApi = 'https://fernando-herrera.com';
+console.log( charmander );
+
